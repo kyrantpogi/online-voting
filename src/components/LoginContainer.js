7883f1a56c07ee25e27fa2c8ee08840479141e73
kyrant <johnkyrant.mendoza@gmail.com>
2022-05-16 21:09:36 +0800

    fixed routes

---
+++ src/components/LoginContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LoginContainer extends React.Component {
+  render() {
+    return (
+      <div className="login-container">
+
+      </div>
+    );
+  }
+}
+
+export default LoginContainer;
