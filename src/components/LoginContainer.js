dbc38bdf84ad0554cd8f2e9007fd41d6544a6b71
kyrant <johnkyrant.mendoza@gmail.com>
2022-05-20 13:24:28 +0800

    styled login

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -3,9 +3,18 @@ import React from 'react';
 class LoginContainer extends React.Component {
   render() {
     return (
-      <div className="login-container">
-
-      </div>
+      <form className="login-container">
+        <h1>Login</h1>
+        <div className="login-form">
+          <input type="text" placeholder="Username"/>
+        </div>
+        <div className="login-form">
+          <input type="text" placeholder="Password"/>
+        </div>
+        <div className="login-form btn">
+          <p>Submit</p>
+        </div>
+      </form>
     );
   }
 }
